cfb7194e531361344afbb03474a21a7a57799cca
JesusTrueclap <54335995+JesusTrueclap@users.noreply.github.com>
2020-02-22 17:47:14 -0800

    Update validatedata.py
    -Correct pressure for time is now added.

--- validatedata.py
+++ validatedata.py
@@ -1,6 +1,6 @@
 import io
 import csv
-
+import traceback
 
 def checkFileExists(fn):
     try:
@@ -9,8 +9,16 @@ def checkFileExists(fn):
     except IOError:
         print("Error: File does not appear to exist.")
         return 0
-
-
+# This returns the avg for a total number and the number of items 
+def getAvg(numList):
+    try: 
+        avg = sum(numList)/len(numList)
+        return avg
+    except:
+        traceback.print_exc()
+        print('ERROR: There was an issue getting the average')
+        return 0
+    
 def main():
     print('About to try to read in file...')
     # filename = input('What is the file name?')
@@ -24,6 +32,10 @@ def main():
         # first lets read in the file 
         # This is used to skip the first line because of the headers. 
         next(csv_file)
+        # total of all the pressure in the data. 
+        pressureDataList = []
+        pressureHeaderName = "Pressure(psig)"
+        timeHeaderName = "Time(ms)"
         # Read in the file with the columns and row accessible 
         csv_reader = csv.DictReader(csv_file, delimiter='\t')
         line_count = 0
@@ -34,7 +46,12 @@ def main():
                 line_count += 1
             else:
                 # print(f'\t{row[0]}')
+                currentTime = float(row[timeHeaderName])
+                if currentTime >= 2000 and currentTime <= 8000:
+                    pressureDataList.append(row[pressureHeaderName])
+                    print(f'\t Time: {row["Time(ms)"]} Pressure: {row["Pressure(psig)"]}')
                 line_count += 1
+        print(f'\t Here is the average found {getAvg(pressureDataList)}')
 
 
 main()
